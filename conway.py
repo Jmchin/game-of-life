843365c394ecdce0be75c19907b51082e2ce0694
Justin Chin <justinmichaelchin@gmail.com>
2017-01-06 11:30:13 -0800

    formats code according to PEP8

--- conway.py
+++ conway.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-import tdl
-from numpy import array, random
-
 """
 An implementation of Conway's Game of Life
 using tdl as the graphical library.
@@ -14,6 +11,10 @@ TODO:
         * example structures
     * render a new window to keep track of current generation
 """
+
+import tdl
+from numpy import array, random
+
 
 WIDTH = 60
 HEIGHT = 60
@@ -27,11 +28,11 @@ OFF = 0
 STATES = [ON, OFF]
 
 def random_grid(height, width):
-    """instantiates a non-uniform random grid of ON/OFF cells"""
+    """Instantiates a non-uniform random grid of ON/OFF cells."""
     return random.choice(STATES, (height * width), p=[0.2, 0.8]).reshape(height, width)
 
 def render(root, con, cells):
-    """renders the world of cells depending on their state"""
+    """Renders the world of cells depending on their state."""
     for x in range(WIDTH):
         for y in range(HEIGHT):
             if cells[x, y] == 1:
@@ -42,27 +43,27 @@ def render(root, con, cells):
     root.blit(con, 0, 0, WIDTH, HEIGHT, 0, 0)
 
 def update(cells):
-    """updates the cell state using conway's rules"""
+    """Updates the cell state using conway's rules."""
     new_cells = cells.copy()
 
     for x in range(WIDTH):
         for y in range(HEIGHT):
-            # adds total of neighbors that are ON
-            neighbors = int(cells[(x-1)%WIDTH, (y-1)%WIDTH] + cells[x, (y-1)%WIDTH] + cells[(x+1)%WIDTH, (y-1)%WIDTH] +
-                           cells[(x-1)%WIDTH, y] + cells[(x+1)%WIDTH, y] +
-                           cells[(x-1)%WIDTH, (y+1)%WIDTH] + cells[x, (y+1)%WIDTH] + cells[(x+1)%WIDTH, (y+1)%WIDTH])
+            # Sums the total of neighbors that are ON
+            neighbors = int(cells[(x-1)%WIDTH, (y-1)%WIDTH] + cells[x, (y-1)%WIDTH] + cells[(x+1)%WIDTH, (y-1)%WIDTH]
+                            + cells[(x-1)%WIDTH, y] + cells[(x+1)%WIDTH, y]
+                            + cells[(x-1)%WIDTH, (y+1)%WIDTH] + cells[x, (y+1)%WIDTH] + cells[(x+1)%WIDTH, (y+1)%WIDTH])
 
             if cells[x,y] == ON:
                 if (neighbors < 2) or (neighbors > 3):
-                    new_cells[x,y] = OFF
+                    new_cells[x, y] = OFF
             else:
                 if neighbors == 3:
-                    new_cells[x,y] = ON
+                    new_cells[x, y] = ON
 
     cells[:] = new_cells[:]
 
 def main():
-    """main program loop"""
+    """The main program loop."""
     root = tdl.init(HEIGHT, WIDTH, 'Game-of-Life')
     con = tdl.Console(HEIGHT, WIDTH)
     tdl.set_fps(10)
